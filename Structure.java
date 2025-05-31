77ccda599133441b0cdc25cc6cef0f4b49887b84
VanshKothari16 <vansh.kothari16@gmail.com>
2025-05-31 16:02:22 +0530

    updated Structure.java code

--- Structure.java
+++ Structure.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.*;
 
 public class Structure extends Connection implements ActionListener {
     JButton sip,normal,cagr,emi;
